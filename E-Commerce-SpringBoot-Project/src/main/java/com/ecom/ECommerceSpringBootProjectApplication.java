d484e2754abf46aec5cdbf42031107d9005415e2
bsile <bsile@Abhishek.mshome.net>
2024-12-28 23:35:50 +0530

    first commit

---
+++ E-Commerce-SpringBoot-Project/src/main/java/com/ecom/ECommerceSpringBootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSpringBootProjectApplication.class, args);
+		System.out.println("Running.....");
+	}
+
+}
